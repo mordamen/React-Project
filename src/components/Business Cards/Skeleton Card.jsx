c3cf2988c495f6a0bd13ebd95e89fbe867e5510a
Mordamen <43504758+Mordamen@users.noreply.github.com>
2024-07-09 21:06:57 +0300

    tiny change to the skeleton cards

--- src/components/Business Cards/Skeleton Card.jsx
+++ src/components/Business Cards/Skeleton Card.jsx
@@ -2,26 +2,27 @@ import { Box, Skeleton } from '@mui/material';
 import Grid from '@mui/material/Unstable_Grid2';
 
 const CardPreview = () => {
-    const skeletonCount = 5;
+	const skeletonCount = 6;
 
-    return (
-        <Grid container spacing={2} className='card-gallery-container' columns={{ xs: 1, sm: 2, md: 4 }} >
-        {Array.from({ length: skeletonCount }, (_, index) => (
-            <Grid key={index} >
-            <Box>
-                <Skeleton
-                animation="wave"
-                variant="rounded"
-                sx={{
-                    bgcolor: 'grey.900',
-                    width: { xs: 270, sm: 360, md: 480 }, height: { xs: 180, sm: 240, md: 320},
-                }}
-                />
-            </Box>
-            </Grid>
-        ))}
-        </Grid>
-    );
+	return (
+		<Grid container spacing={2} className='card-gallery-container' columns={{ xs: 1, sm: 2, md: 4 }}>
+			{Array.from({ length: skeletonCount }, (_, index) => (
+				<Grid key={index}>
+					<Box>
+						<Skeleton
+							animation='wave'
+							variant='rounded'
+							sx={{
+								bgcolor: 'grey.900',
+								width: { xs: 270, sm: 360, md: 480 },
+								height: { xs: 180, sm: 240, md: 320 },
+							}}
+						/>
+					</Box>
+				</Grid>
+			))}
+		</Grid>
+	);
 };
 
 export default CardPreview;
--- src/components/General Components/Text Blocks.jsx
+++ src/components/General Components/Text Blocks.jsx
@@ -1,27 +1,27 @@
-import { Grid } from '@mui/material'
-import InputField from './InputField'
+import { Grid } from '@mui/material';
+import InputField from './InputField';
 
-const TextBlocks = ({initializeTextFields, inputState, errorState, showErrors, handleInputChange,}) => {
-
-    return (
-        <Grid container spacing={2}>
-            {initializeTextFields && Object.keys(initializeTextFields).map((key) => (
-                <InputField
-                key={key + 'inputs'}
-                label={initializeTextFields[key].label}
-                type={initializeTextFields[key].type}
-                id={key}
-                cols={initializeTextFields[key].sm}
-                required={initializeTextFields[key].required}
-                size='small'
-                helperText={errorState && errorState[key]}
-                value={inputState[key]}
-                onChange={handleInputChange}
-                showErrors={showErrors}
-                />
-            ))}
-        </Grid>
-    )
-}
+const TextBlocks = ({ initializeTextFields, inputState, errorState, showErrors, handleInputChange }) => {
+	return (
+		<Grid container spacing={2}>
+			{initializeTextFields &&
+				Object.keys(initializeTextFields).map((key) => (
+					<InputField
+						key={key + 'inputs'}
+						label={initializeTextFields[key].label}
+						type={initializeTextFields[key].type}
+						id={key}
+						cols={initializeTextFields[key].sm}
+						required={initializeTextFields[key].required}
+						size='small'
+						helperText={errorState && errorState[key]}
+						value={inputState[key]}
+						onChange={handleInputChange}
+						showErrors={showErrors}
+					/>
+				))}
+		</Grid>
+	);
+};
 
 export default TextBlocks;
